6f2ae4d2b13a2df256ee17d0d8d5e7c3590686d5
Scott Hutley <scotthutley1@comcast.net>
2021-09-20 14:13:59 -0700

    Add edgecase for NaN and negative
    Co-authored-by: Anna Clarke <annac.klingberg@gmail.com>

--- Program.cs
+++ Program.cs
@@ -8,16 +8,19 @@ namespace Game {
     public static void Main()
     {
       
-      Console.WriteLine("Enter a number:  or enter q to quit");
+      Console.WriteLine("Enter a number:  [or enter q to quit]");
       string stringNumber = Console.ReadLine();
+      int number;
       if (stringNumber == "q" || stringNumber == "Q") {
         Console.WriteLine("Goodbye");
-      } else {
-        int number = int.Parse(stringNumber);
+      } else if (int.TryParse(stringNumber, out number) && int.Parse(stringNumber) > 0) {
         List<string> pongList = PingPong.MakePongList(number);
         foreach (string item in pongList) {
           Console.WriteLine(item);
         }
+        Main();
+      } else {
+        Console.WriteLine("Sorry, cannot read input. Please enter a positive integer or q to quit");
         Main();
       }
     }
